fc693b68af88678625550cd6c3990e9c5cc1e842
Muhammad Anis <anistech.dev@gmail.com>
2024-11-14 20:56:31 +0500

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import adminRegisterPage from "./Pages/Admin/register";
 import './Pages/Admin/assets/css/pace.min.css'
 import './Pages/Admin/assets/plugins/perfect-scrollbar/css/perfect-scrollbar.css'
 import './Pages/Admin/assets/plugins/metismenu/metisMenu.min.css'
@@ -9,13 +8,18 @@ import './Pages/Admin/assets/css/bootstrap-extended.css'
 import './Pages/Admin/assets/sass/main.css'
 import './Pages/Admin/assets/sass/dark-theme.css'
 import './Pages/Admin/assets/sass/responsive.css'
+import AdminRegisterPage from "./Pages/Admin/register";
+import AdminLoginPage from "./Pages/Admin/Login";
+import IndexAdmin from "./Pages/Admin/Index";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/admin/register" Component={adminRegisterPage} />
+          <Route path="/admin/register" Component={AdminRegisterPage} />
+          <Route path="/admin/login" Component={AdminLoginPage} />
+          <Route path="/admin" Component={IndexAdmin} />
         </Routes>
       </BrowserRouter>
     </>
